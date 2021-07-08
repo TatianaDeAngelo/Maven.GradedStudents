15bda8b0f7b046b634fe5ba7b974254edecd5681
TatianaDeAngelo <85692272+TatianaDeAngelo@users.noreply.github.com>
2021-07-08 13:28:46 -0400

    Just started

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> testScores; //initally said examScores but then continued to refer to it as testScores, so I changed it
+
+    public Student (String firstName, String lastName, Double[] testScores) {
+
+
+    }
+
 }
